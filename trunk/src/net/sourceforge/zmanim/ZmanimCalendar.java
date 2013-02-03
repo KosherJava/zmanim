cee3b3e7908a8c22f6d81a02efd8ef0a4f771269
ehershfeld@gmail.com <ehershfeld@gmail.com@17d67dbf-1970-d304-196b-cc87bb79cbd8>
2013-02-03 06:23:13 +0000

    Copyright year update for the previous commit.

--- trunk/src/net/sourceforge/zmanim/ZmanimCalendar.java
+++ trunk/src/net/sourceforge/zmanim/ZmanimCalendar.java
@@ -1,6 +1,6 @@
 /*
  * Zmanim Java API
- * Copyright (C) 2004-2012 Eliyahu Hershfeld
+ * Copyright (C) 2004-2013 Eliyahu Hershfeld
  *
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
@@ -30,8 +30,7 @@ import net.sourceforge.zmanim.util.GeoLocation;
  * <h2>Disclaimer:</h2> I did my best to get accurate results but please do not rely on these zmanim for
  * <em>halacha lemaaseh</em>.
  * 
- * @author &copy; Eliyahu Hershfeld 2004 - 2012
- * @version 1.2.1
+ * @author &copy; Eliyahu Hershfeld 2004 - 2013
  */
 public class ZmanimCalendar extends AstronomicalCalendar {
 
--- trunk/src/net/sourceforge/zmanim/util/AstronomicalCalculator.java
+++ trunk/src/net/sourceforge/zmanim/util/AstronomicalCalculator.java
@@ -1,6 +1,6 @@
 /*
  * Zmanim Java API
- * Copyright (C) 2004-2011 Eliyahu Hershfeld
+ * Copyright (C) 2004-2013 Eliyahu Hershfeld
  *
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
@@ -22,8 +22,7 @@ import java.util.Calendar;
  * runtime, easily allowing comparison the results of using different algorithms. TODO: consider methods that would
  * allow atmospheric modeling. This can currently be adjusted by {@link #setRefraction(double) setting the refraction}.
  * 
- * @author &copy; Eliyahu Hershfeld 2004 - 2011
- * @version 1.1
+ * @author &copy; Eliyahu Hershfeld 2004 - 2013
  */
 public abstract class AstronomicalCalculator implements Cloneable {
 	/**
