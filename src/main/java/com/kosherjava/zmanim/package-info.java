200b458ea476a2be24d3df320daf615891283191
KosherJava <KosherJava@users.noreply.github.com>
2020-10-04 20:51:53 -0400

    remove stray * causing a JavaDoc issue
    Props to @elijulian for this and the h3 issue report.

--- src/main/java/com/kosherjava/zmanim/package-info.java
+++ src/main/java/com/kosherjava/zmanim/package-info.java
@@ -3,7 +3,7 @@
  * astronomical times including sunrise and sunset and Jewish <em><a href="https://en.wikipedia.org/wiki/Zmanim">zmanim</a></em> or religious
  * times for prayers and other Jewish religious duties. These classes extend the {@link java.util.GregorianCalendar} and can therefore use the
  * standard Calendar functionality to change dates etc. For non religious astronomical / solar calculations such as <a href=
- * "https://en.wikipedia.org/wiki/Sunrise">sunrise</a>, <a href= * "https://en.wikipedia.org/wiki/Sunset">sunset</a> and <a href=
+ * "https://en.wikipedia.org/wiki/Sunrise">sunrise</a>, <a href="https://en.wikipedia.org/wiki/Sunset">sunset</a> and <a href=
  * "https://en.wikipedia.org/wiki/Twilight">twilight</a>, use the {@link AstronomicalCalendar}. The {@link ZmanimCalendar} contains the most
  * commonly used zmanim or religious time calculations. For a much more extensive list of <em>zmanim</em> use the {@link ComplexZmanimCalendar}.
  * <b>Note:</b> It is important to read the technical notes on top of the {@link com.kosherjava.zmanim.util.AstronomicalCalculator} documentation.
