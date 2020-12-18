b5567193b87cc7ba0e99f73fc23faf649bad5c60
plonibarploni <44826203+plonibarploni@users.noreply.github.com>
2020-12-17 23:02:03 -0500

    add more accurate implementation of Vesein Tal Umatar/Vesein Berachah
    based on https://github.com/yparitcher/libzmanim/blob/65d0ba20d98e95a771fb708ad2e58eb91ff6aae6/src/hebrewcalendar.c#L883-L930

--- src/main/java/com/kosherjava/zmanim/hebrewcalendar/JewishCalendar.java
+++ src/main/java/com/kosherjava/zmanim/hebrewcalendar/JewishCalendar.java
@@ -241,6 +241,47 @@ public class JewishCalendar extends com.kosherjava.zmanim.hebrewcalendar.JewishD
 	}
 
 	/**
+	 * Tekufas Shmuel: a solar year is 365.25 days.
+	 * notation: days,hours,chalakim
+	 * molad BaHaRad was 2D,5H,204C
+	 * or 5H,204C from the start of rosh hashana year 1
+	 * molad nissan add 177D,4H,438C (6 * 29D,12H,793C)
+	 * or 177D,9H,642C after rosh hashana year 1
+	 * tekufas nissan was 7D,9H,642C before molad nissan ~rambam.
+	 * or 170D,0H,0C after rosh hashana year 1
+	 * tekufas tishrei was 182D,3H (365.25 / 2) before tekufas nissan
+	 * or 12D,15H before Rosh Hashana year 1
+	 * outside of EY we say תל ומטר in ברכת השנים from 60 days after tekufas tishrei.
+	 * 60 includes the day of the tekufah and the day we start.
+	 * 60 days from the tekufah == 47D,9H from Rosh Hashana year 1
+	 */
+	public int tekufasTishreiElapsedDays() {
+		// days since Rosh Hashana year 1
+		// add 1/2 day as the first tekufas tishrei was 9 hours into the day
+		// this allows all 4 years of the secular leap year cycle to share 47 days
+		// make from 47D,9H to 47D for simplicity
+		double days = getJewishCalendarElapsedDays(getJewishYear()) + (getDaysSinceStartOfJewishYear()-1) + .5;
+		// days of completed solar years
+		double solar = (getJewishYear()-1)*365.25;
+		return (int) (days - solar);
+	}
+
+	public boolean isBirchasHashanimToday() {
+		if (inIsrael && getJewishMonth() == 7 && getJewishDayOfMonth() == 7) return true;
+		else return tekufasTishreiElapsedDays() == 47;
+	}
+
+	public boolean getBirchasHashanim() {
+		if (getJewishMonth() == 1 && getJewishDayOfMonth() < 15) return true;
+		if (getJewishMonth() < 7) return false;
+		if (inIsrael) {
+			return getJewishMonth() != 7 || getJewishDayOfMonth() >= 7;
+		} else {
+			return tekufasTishreiElapsedDays() >= 47;
+		}
+	}
+
+	/**
 	 * Return the type of year for parsha calculations. The algorithm follows the
 	 * <a href="http://hebrewbooks.org/pdfpager.aspx?req=14268&amp;st=&amp;pgnum=222">Luach Arba'ah Shearim</a> in the Tur Ohr Hachaim.
 	 * @return the type of year for parsha calculations.
