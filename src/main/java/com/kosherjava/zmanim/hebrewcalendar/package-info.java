5730f99a169540e712cc8fe31e89d49ce9e4e25f
KosherJava <KosherJava@users.noreply.github.com>
2021-12-03 14:31:22 -0500

    Update to add source for additional season-based
    davening code from @yparitcher
    Update the copyright year

--- src/main/java/com/kosherjava/zmanim/hebrewcalendar/package-info.java
+++ src/main/java/com/kosherjava/zmanim/hebrewcalendar/package-info.java
@@ -2,7 +2,7 @@
  * This package contain classes that represent a <a href="https://en.wikipedia.org/wiki/Hebrew_calendar">Jewish Date/Calendar</a>,
  * and allows conversion between {@link JewishDate Jewish} and {@link java.util.GregorianCalendar Gregorian dates}. The main calendar
  * classes {@link JewishCalendar} and {@link JewishDate} are based on <a href="http://www.facebook.com/avromf">Avrom Finkelstien's</a> code,
- * refactored to fit the Zmanim API. The parsha code was ported by Y. Paritcher from his
+ * refactored to fit the Zmanim API. The parsha and season-based davening change code was ported by Y. Paritcher from his
  * <a href="https://github.com/yparitcher/libzmanim">libzmanim</a> code.
  * 
  * <h2>Design:</h2>
@@ -12,6 +12,6 @@
  *   <li>{@link HebrewDateFormatter} defines the basics for taking a JewishCalendar and formatting the dates.
  *   <li>{@link YomiCalculator} calculates the {@link Daf} Yomi Bavli for a given JewishCalendar and {@link YerushalmiYomiCalculator}.
  * </ul>
- * @author &copy; Eliyahu Hershfeld 2011 - 2020
+ * @author &copy; Eliyahu Hershfeld 2011 - 2021
  */
 package com.kosherjava.zmanim.hebrewcalendar;
