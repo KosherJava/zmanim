81a0e64ea86885fe8128d4517a0df872e2ec4eaa
KosherJava <KosherJava@users.noreply.github.com>
2022-01-04 19:20:34 -0500

    Update package-info.java
    with a minor tweak to reference Jean Meeus

--- src/main/java/com/kosherjava/zmanim/util/package-info.java
+++ src/main/java/com/kosherjava/zmanim/util/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Utility classes for the Zmanim API including classes to calculate sun based times, {@link GeoLocation location information},
  * some {@link ZmanimFormatter formatting} and {@link GeoLocationUtils geographic location utilities}. Included in this package
- * are implementations for both the {@link SunTimesCalculator USNO} and {@link NOAACalculator NOAA} algorithms.
+ * are implementations for both the {@link SunTimesCalculator USNO} and {@link NOAACalculator NOAA / Jean Meeus} algorithms.
  * 
- * @author &copy; Eliyahu Hershfeld 2004 - 2020
+ * @author &copy; Eliyahu Hershfeld 2004 - 2022
  */
 package com.kosherjava.zmanim.util;
