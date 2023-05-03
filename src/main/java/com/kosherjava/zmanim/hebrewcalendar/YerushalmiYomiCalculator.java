5162a97b379e3231ce7240a3cea3a3471414a15d
KosherJava <KosherJava@users.noreply.github.com>
2023-05-03 15:52:55 -0400

    Return proper Daf Yomi Yerushalmi
    Thanks @elazarrosenthal
    Closes #207

--- src/main/java/com/kosherjava/zmanim/hebrewcalendar/YerushalmiYomiCalculator.java
+++ src/main/java/com/kosherjava/zmanim/hebrewcalendar/YerushalmiYomiCalculator.java
@@ -1,16 +1,16 @@
 /*
  * Zmanim Java API
- * Copyright (C) 2017 - 2021 Eliyahu Hershfeld
+ * Copyright (C) 2017 - 2023 Eliyahu Hershfeld
  *
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
  *
  * This library is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA,
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA,
  * or connect to: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
  */
 package com.kosherjava.zmanim.hebrewcalendar;
@@ -24,7 +24,7 @@ import java.util.GregorianCalendar;
  * "https://en.wikipedia.org/wiki/Daf_Yomi">Daf Yomi</a> page ({@link Daf}) for the a given date.
  * 
  * @author &copy; elihaidv
- * @author &copy; Eliyahu Hershfeld 2017 - 2021
+ * @author &copy; Eliyahu Hershfeld 2017 - 2023
  */
 public class YerushalmiYomiCalculator {
 	
@@ -97,7 +97,7 @@ public class YerushalmiYomiCalculator {
 		// Finally find the daf.
 		for (int j = 0; j < BLATT_PER_MASSECTA.length; j++) {
 			
-			if (total <= BLATT_PER_MASSECTA[j]) {
+			if (total < BLATT_PER_MASSECTA[j]) {
 				dafYomi = new Daf(masechta, total + 1);
 				break;
 			}
