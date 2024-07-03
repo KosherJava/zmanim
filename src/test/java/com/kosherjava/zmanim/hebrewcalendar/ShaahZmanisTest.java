2ecf4e693009e2a7f5327efcf8c374aa7ab60a74
M. Waisberg <pnemonic@gmail.com>
2024-07-03 09:12:16 +0300

    Shaah Zmanis type

---
+++ src/test/java/com/kosherjava/zmanim/hebrewcalendar/ShaahZmanisTest.java
@@ -0,0 +1,127 @@
+package com.kosherjava.zmanim.hebrewcalendar;
+
+import static org.junit.Assert.assertTrue;
+
+import com.kosherjava.zmanim.ComprehensiveZmanimCalendar;
+import com.kosherjava.zmanim.ShaahZmanis;
+import com.kosherjava.zmanim.ZmanimCalendar;
+import com.kosherjava.zmanim.util.GeoLocation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+public class ShaahZmanisTest {
+
+	@Test
+	public void testTemporalHour() {
+		ZmanimCalendar calendar = new ComprehensiveZmanimCalendar();
+		long hour;
+
+		hour = calendar.getTemporalHour();
+		assertTrue(hour > 0);
+		hour = calendar.getShaahZmanis();
+		assertTrue(hour > 0);
+	}
+
+	@Test
+	public void testShaahZmanis() {
+		GeoLocation location = new GeoLocation("test", 0.0, 0.0, ZoneId.of("UTC"));
+		ComprehensiveZmanimCalendar calendar = new ComprehensiveZmanimCalendar(location);
+		calendar.setLocalDate(LocalDate.of(2022, Month.JUNE, 22));
+		long hour;
+		long delta = 1000;
+
+		hour = calendar.getShaahZmanis120Minutes();
+		assertEquals(4836877, hour, delta);
+		hour = calendar.getShaahZmanis120MinutesZmanis();
+		assertEquals(4849169, hour, delta);
+		hour = calendar.getShaahZmanis16Point1Degrees();
+		assertEquals(4304341, hour, delta);
+		hour = calendar.getShaahZmanis18Degrees();
+		assertEquals(4387928, hour, delta);
+		hour = calendar.getShaahZmanis19Point8Degrees();
+		assertEquals(4467292, hour, delta);
+		hour = calendar.getShaahZmanis26Degrees();
+		assertEquals(4742324, hour, delta);
+		hour = calendar.getShaahZmanis60Minutes();
+		assertEquals(4236877, hour, delta);
+		hour = calendar.getShaahZmanis72Minutes();
+		assertEquals(4356877, hour, delta);
+		hour = calendar.getShaahZmanis72MinutesZmanis();
+		assertEquals(4364252, hour, delta);
+		hour = calendar.getShaahZmanis90Minutes();
+		assertEquals(4536877, hour, delta);
+		hour = calendar.getShaahZmanis90MinutesZmanis();
+		assertEquals(4546096, hour, delta);
+		hour = calendar.getShaahZmanis96Minutes();
+		assertEquals(4596877, hour, delta);
+		hour = calendar.getShaahZmanis96MinutesZmanis();
+		assertEquals(4606710, hour, delta);
+		hour = calendar.getShaahZmanisAteretTorah();
+		assertEquals(4200564, hour, delta);
+		hour = calendar.getShaahZmanisBaalHatanya();
+		assertEquals(3669565, hour, delta);
+		hour = calendar.getShaahZmanisGRA();
+		assertEquals(3636877, hour, delta);
+		hour = calendar.getShaahZmanis72Minutes();
+		assertEquals(4356877, hour, delta);
+	}
+
+	@Test
+	public void testShaahZmanisType() {
+		GeoLocation location = new GeoLocation("test", 0.0, 0.0, ZoneId.of("UTC"));
+		ComprehensiveZmanimCalendar calendar = new ComprehensiveZmanimCalendar(location);
+		calendar.setLocalDate(LocalDate.of(2022, Month.JUNE, 22));
+		long hour;
+		long delta = 1000;
+
+		calendar.setShaahZmanisType(ShaahZmanis.MINUTES_120);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4836877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.DEGREES_16POINT1);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4304341, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.DEGREES_18);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4387928, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.DEGREES_19POINT8);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4467292, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.DEGREES_26);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4742324, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.MINUTES_60);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4236877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.MINUTES_72);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4356877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.MINUTES_90);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4536877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.MINUTES_96);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4596877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.ATERET);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4200564, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.BAAL_HATANYA);
+		hour = calendar.getShaahZmanis();
+		assertEquals(3669565, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.GRA);
+		hour = calendar.getShaahZmanis();
+		assertEquals(3636877, hour, delta);
+		calendar.setShaahZmanisType(ShaahZmanis.MGA);
+		hour = calendar.getShaahZmanis();
+		assertEquals(4356877, hour, delta);
+	}
+
+	private static void assertEquals(long expected, long actual, long delta) {
+		Assert.assertEquals((float) expected, (float) actual, (float) delta);
+	}
+}
