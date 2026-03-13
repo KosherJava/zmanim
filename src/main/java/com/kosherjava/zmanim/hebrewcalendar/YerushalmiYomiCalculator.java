8b9dba0053c67de7025c520e2a049162e2b96370
KosherJava <KosherJava@users.noreply.github.com>
2026-03-13 18:02:18 -0400

    Start using java.time classes

--- src/main/java/com/kosherjava/zmanim/hebrewcalendar/YerushalmiYomiCalculator.java
+++ src/main/java/com/kosherjava/zmanim/hebrewcalendar/YerushalmiYomiCalculator.java
@@ -15,9 +15,8 @@
  */
 package com.kosherjava.zmanim.hebrewcalendar;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * This class calculates the <a href="https://en.wikipedia.org/wiki/Jerusalem_Talmud">Talmud Yerusalmi</a> <a href=
@@ -27,149 +26,136 @@ import java.util.GregorianCalendar;
  * @author &copy; Eliyahu Hershfeld 2017 - 2025
  */
 public class YerushalmiYomiCalculator {
-	
-	/**
-	 * The start date of the first Daf Yomi Yerushalmi cycle of February 2, 1980 / 15 Shevat, 5740.
-	 */
-	private final static Calendar DAF_YOMI_START_DAY = new GregorianCalendar(1980, Calendar.FEBRUARY, 2);
-	/** The number of milliseconds in a day. */
-	private final static int DAY_MILIS = 1000 * 60 * 60 * 24;
-	/** The number of pages in the Talmud Yerushalmi.*/
-	private final static int WHOLE_SHAS_DAFS = 1554;
-	/** The number of pages per <em>masechta</em> (tractate).*/
-	private final static int[] BLATT_PER_MASECHTA = { 
-			68, 37, 34, 44, 31, 59, 26, 33, 28, 20, 13, 92, 65, 71, 22, 22, 42, 26, 26, 33, 34, 22,
-			19, 85, 72, 47, 40, 47, 54, 48, 44, 37, 34, 44, 9, 57, 37, 19, 13};
 
-	/**
-	 * Default constructor.
-	 */
-	public YerushalmiYomiCalculator() {
-		// nothing here
-	}
-	
-	/**
-	 * Returns the <a href="https://en.wikipedia.org/wiki/Daf_Yomi">Daf Yomi</a>
-	 * <a href="https://en.wikipedia.org/wiki/Jerusalem_Talmud">Yerusalmi</a> page ({@link Daf}) for a given date.
-	 * The first Daf Yomi cycle started on 15 Shevat (Tu Bishvat), 5740 (February, 2, 1980) and calculations
-	 * prior to this date will result in an IllegalArgumentException thrown. A null will be returned on Tisha B'Av or
-	 * Yom Kippur.
-	 *
-	 * @param calendar
-	 *            the calendar date for calculation
-	 * @return the {@link Daf} or null if the date is on Tisha B'Av or Yom Kippur.
-	 *
-	 * @throws IllegalArgumentException
-	 *             if the date is prior to the February 2, 1980, the start of the first Daf Yomi Yerushalmi cycle
-	 */
-	public static Daf getDafYomiYerushalmi(JewishCalendar calendar) {
-		
-		Calendar nextCycle = new GregorianCalendar();
-		Calendar prevCycle = new GregorianCalendar();
-		Calendar requested = calendar.getGregorianCalendar();
-		int masechta = 0;
-		Daf dafYomi = null;
+    /** Start date of first Daf Yomi Yerushalmi cycle: 1980-02-02 */
+    private static final ZonedDateTime DAF_YOMI_START_DAY =
+            ZonedDateTime.of(1980, 2, 2, 0, 0, 0, 0, ZoneId.systemDefault());
 
-		// There isn't Daf Yomi on Yom Kippur or Tisha B'Av.
-		if ( calendar.getYomTovIndex() == JewishCalendar.YOM_KIPPUR ||
-				calendar.getYomTovIndex() == JewishCalendar.TISHA_BEAV ) {
-			return null;
-		}
-		
-		
-		if (requested.before(DAF_YOMI_START_DAY)) {
-			throw new IllegalArgumentException(requested + " is prior to organized Daf Yomi Yerushalmi cycles that started on "
-					+ DAF_YOMI_START_DAY);
-		}
-		
-		// Start to calculate current cycle. init the start day
-		nextCycle.setTime(DAF_YOMI_START_DAY.getTime());
-		
-		// Go cycle by cycle, until we get the next cycle
-		while (requested.after(nextCycle)) {
-			prevCycle.setTime(nextCycle.getTime());
-			
-			// Adds the number of whole shas dafs. and the number of days that not have daf.
-			nextCycle.add(Calendar.DAY_OF_MONTH, WHOLE_SHAS_DAFS);
-			nextCycle.add(Calendar.DAY_OF_MONTH, getNumOfSpecialDays(prevCycle, nextCycle));		
-		}
-		
-		// Get the number of days from cycle start until request.
-		int dafNo = (int)(getDiffBetweenDays(prevCycle, requested));
-		
-		// Get the number of special day to subtract
-		int specialDays = getNumOfSpecialDays(prevCycle, requested);
-		int total = dafNo - specialDays;
-				
-		// Finally find the daf.
-		for (int i : BLATT_PER_MASECHTA) {
-			if (total < i) {
-				dafYomi = new Daf(masechta, total + 1);
-				break;
-			}
-			total -= i;
-			masechta++;
-		}
+    /** milliseconds in a day*/
+    private static final int DAY_MILLIS = 1000 * 60 * 60 * 24;
+    /** number of pages (<em>blatt/dafim</em>) in <em>Shas</em>*/
+    private static final int WHOLE_SHAS_DAFS = 1554;
+    
+    /** number of pages (<em>blatt/dafim</em>) per <em>masechta</em>*/
+    private static final int[] BLATT_PER_MASECHTA = {
+            68, 37, 34, 44, 31, 59, 26, 33, 28, 20, 13, 92, 65, 71, 22, 22, 42, 26, 26, 33, 34, 22,
+            19, 85, 72, 47, 40, 47, 54, 48, 44, 37, 34, 44, 9, 57, 37, 19, 13
+    };
 
-		return dafYomi;
-	}
-	
-	/**
-	 * Return the number of special days (Yom Kippur and Tisha Beav, where there are no dafim), between the start date
-	 * (as a <code>Calendar</code>) and end date (also as a <code>Calendar</code>).
-	 * 
-	 * @param start date to start calculating from
-	 * @param end date to finish calculating at
-	 * @return the number of special days between the start and end dates
-	 */
-	private static int getNumOfSpecialDays(Calendar start, Calendar end) {
-		
-		// Find the start and end Jewish years
-		int startYear = new JewishCalendar(start).getJewishYear();
-		int endYear = new JewishCalendar(end).getJewishYear();
-		
-		// Value to return
-		int specialDays = 0;
-		
-		//Instant of special Dates
-		JewishCalendar yom_kippur = new JewishCalendar(5770, 7, 10);
-		JewishCalendar tisha_beav = new JewishCalendar(5770, 5, 9);
+    /**
+     * Default constructor.
+     */
+    public YerushalmiYomiCalculator() {
+        // No-op
+    }
 
-		// Go over the years and find special dates
-		for (int i = startYear; i <= endYear; i++) {
-			yom_kippur.setJewishYear(i);
-			tisha_beav.setJewishYear(i);
-			
-			if (isBetween(start, yom_kippur.getGregorianCalendar(), end)) {
-				specialDays++;
-			}
-			if (isBetween(start, tisha_beav.getGregorianCalendar(), end)) {
-				specialDays++;
-			}
-		}
-		
-		return specialDays;
-	}
+    /**
+     * Returns the Daf Yomi Yerushalmi page for a given Jewish date.
+     * Returns null on Yom Kippur or Tisha B’Av.
+     * @param jewishCalendar the <code>JewishCalendar</code> to set.
+     * @return the daf.
+     */
+    public static Daf getDafYomiYerushalmi(JewishCalendar jewishCalendar) {
+        ZonedDateTime requested = jewishCalendar.getGregorianCalendar().toInstant()
+                                          .atZone(ZoneId.systemDefault());
+        int masechta = 0;
+        Daf dafYomi = null;
 
-	/**
-	 * Return if the date is between two dates
-	 * 
-	 * @param start the start date
-	 * @param date the date being compared
-	 * @param end the end date
-	 * @return if the date is between the start and end dates
-	 */
-	private static boolean isBetween(Calendar start, Calendar date, Calendar end ) {
-		return start.before(date) && end.after(date);
-	}
-	
-	/**
-	 * Return the number of days between the dates passed in
-	 * @param start the start date
-	 * @param end the end date
-	 * @return the number of days between the start and end dates
-	 */
-	private static long getDiffBetweenDays(Calendar start, Calendar end) {
-		return  (end.getTimeInMillis() - start.getTimeInMillis()) / DAY_MILIS;
-	}
+        // No Daf Yomi on Yom Kippur or Tisha B'Av
+        if (jewishCalendar.getYomTovIndex() == JewishCalendar.YOM_KIPPUR ||
+                jewishCalendar.getYomTovIndex() == JewishCalendar.TISHA_BEAV) {
+            return null;
+        }
+
+        if (requested.isBefore(DAF_YOMI_START_DAY)) {
+            throw new IllegalArgumentException(requested + " is prior to the first Daf Yomi cycle starting on "
+                    + DAF_YOMI_START_DAY);
+        }
+
+        // Initialize cycle dates
+        ZonedDateTime nextCycle = DAF_YOMI_START_DAY;
+        ZonedDateTime prevCycle = DAF_YOMI_START_DAY;
+
+        // Loop through cycles until we reach the requested date
+        while (requested.isAfter(nextCycle)) {
+            prevCycle = nextCycle;
+
+            // Add whole cycle days
+            nextCycle = nextCycle.plusDays(WHOLE_SHAS_DAFS);
+            nextCycle = nextCycle.plusDays(getNumOfSpecialDays(prevCycle, nextCycle));
+        }
+
+        // Days between start of cycle and requested date
+        int dafNo = (int) getDiffBetweenDays(prevCycle, requested);
+
+        int specialDays = getNumOfSpecialDays(prevCycle, requested);
+        int total = dafNo - specialDays;
+
+        // Find the daf
+        for (int blatt : BLATT_PER_MASECHTA) {
+            if (total < blatt) {
+                dafYomi = new Daf(masechta, total + 1);
+                break;
+            }
+            total -= blatt;
+            masechta++;
+        }
+
+        return dafYomi;
+    }
+
+    /**
+     * Counts the number of "special days" (Yom Kippur, Tisha B’Av) between two ZonedDateTimes.
+     * @param start the start date for the calculation
+     * @param end the start date for the calculatio
+     * @return the number of special days
+     */
+    private static int getNumOfSpecialDays(ZonedDateTime start, ZonedDateTime end) {
+
+        int startYear = new JewishCalendar(start).getJewishYear();
+        int endYear = new JewishCalendar(end).getJewishYear();
+
+        int specialDays = 0;
+
+        // Loop over each Jewish year in range
+        for (int year = startYear; year <= endYear; year++) {
+            // Create Yom Kippur and Tisha B’Av for that Jewish year
+            JewishCalendar yomKippur = new JewishCalendar(5770, 7, 10); // month/day are constants
+            JewishCalendar tishaBeav = new JewishCalendar(5770, 5, 9);
+
+            yomKippur.setJewishYear(year);
+            tishaBeav.setJewishYear(year);
+
+            ZonedDateTime ykDate = yomKippur.getGregorianCalendar().toInstant()
+                                             .atZone(start.getZone());
+            ZonedDateTime tbDate = tishaBeav.getGregorianCalendar().toInstant()
+                                             .atZone(start.getZone());
+
+            if (isBetween(start, ykDate, end)) specialDays++;
+            if (isBetween(start, tbDate, end)) specialDays++;
+        }
+
+        return specialDays;
+    }
+
+    /**
+     * Checks if a date is strictly between start and end.
+     * @param start the start <code>ZonedDateTime</code>
+     * @param date the <code>ZonedDateTime</code> to check
+     * @param end the end <code>ZonedDateTime</code>
+     * @return if the date is between the two dates
+     */
+    private static boolean isBetween(ZonedDateTime start, ZonedDateTime date, ZonedDateTime end) {
+        return start.isBefore(date) && end.isAfter(date);
+    }
+
+    /**
+     * Returns the number of full days between two <code>ZonedDateTime</code>s.
+     * @param start the start <code>ZonedDateTime</code>
+     * @param end the end <code>ZonedDateTime</code>
+     * @return the number of days between the dates.
+     */
+    private static long getDiffBetweenDays(ZonedDateTime start, ZonedDateTime end) {
+        return (end.toInstant().toEpochMilli() - start.toInstant().toEpochMilli()) / DAY_MILLIS;
+    }
 }
