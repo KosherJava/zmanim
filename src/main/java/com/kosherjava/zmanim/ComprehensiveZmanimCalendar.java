9e9131ed69e83eaf36054f836f920f49c23ffb05
KosherJava <KosherJava@users.noreply.github.com>
2026-03-22 01:35:11 -0400

    getMinchaGedolaGreaterThan30() now takes in a minchaGedola param
    - This creates a generic the getMinchaGedolaGreaterThan30(Instant) from what had been a generically named one that returned mincha gedolaGRA  and another one for the Baal Hatanya. This now works for any mincha gedola passed in.
    - Also removed two unused import statements.

--- src/main/java/com/kosherjava/zmanim/ComprehensiveZmanimCalendar.java
+++ src/main/java/com/kosherjava/zmanim/ComprehensiveZmanimCalendar.java
@@ -15,8 +15,6 @@
  */
 package com.kosherjava.zmanim;
 
-import java.time.LocalDate;
-import java.util.Calendar; // FIXME remove once FORWARD can be refactored.
 import java.time.Instant;
 import com.kosherjava.zmanim.util.AstronomicalCalculator;
 import com.kosherjava.zmanim.util.GeoLocation;
@@ -1921,12 +1919,12 @@ public class ComprehensiveZmanimCalendar extends ZmanimCalendar {
 	 * @see #isUseAstronomicalChatzos()
 	 * 
 	 */
-	public Instant getMinchaGedolaGreaterThan30() {
-		if (getMinchaGedola30Minutes() == null || getMinchaGedola() == null) {
+	public Instant getMinchaGedolaGreaterThan30(Instant minchaGedola) {
+		if (getMinchaGedola30Minutes() == null || minchaGedola == null) {
 			return null;
 		} else {
-			return getMinchaGedola30Minutes().compareTo(getMinchaGedola()) > 0 ? getMinchaGedola30Minutes()
-					: getMinchaGedola();
+			return getMinchaGedola30Minutes().compareTo(minchaGedola) > 0 ? getMinchaGedola30Minutes()
+					: minchaGedola;
 		}
 	}
 
@@ -3964,29 +3962,6 @@ public class ComprehensiveZmanimCalendar extends ZmanimCalendar {
 	 */
 	public Instant getMinchaGedolaBaalHatanya() {
 		return getMinchaGedola(getSunriseBaalHatanya(), getSunsetBaalHatanya(), true);
-	}
-
-	/**
-	 * FIXME synchronous
-	 * This is a convenience method that returns the later of {@link #getMinchaGedolaBaalHatanya()} and
-	 * {@link #getMinchaGedola30Minutes()}. In the winter when 1/2 of a {@link #getShaahZmanisBaalHatanya()
-	 * <em>shaah zmanis</em>} is less than 30 minutes {@link #getMinchaGedola30Minutes()} will be returned, otherwise
-	 * {@link #getMinchaGedolaBaalHatanya()} will be returned.
-	 * @todo Consider adjusting this to calculate the time as 30 clock or <em>zmaniyos </em> minutes after either {@link
-	 *         #getSunTransit() astronomical <em>chatzos</em>} or {@link #getChatzosAsHalfDay() <em>chatzos</em> as half a day}
-	 *         for {@link AstronomicalCalculator calculators} that support it, based on {@link #isUseAstronomicalChatzos()}.
-	 * @return the <code>Instant</code> of the later of {@link #getMinchaGedolaBaalHatanya()} and {@link #getMinchaGedola30Minutes()}.
-	 *         If the calculation can't be computed such as in the Arctic Circle where there is at least one day a year
-	 *         where the sun does not rise, and one where it does not set, a <code>null</code> will be returned. See detailed
-	 *         explanation on top of the {@link AstronomicalCalendar} documentation.
-	 */
-	public Instant getMinchaGedolaBaalHatanyaGreaterThan30() {
-		if (getMinchaGedola30Minutes() == null || getMinchaGedolaBaalHatanya() == null) {
-			return null;
-		} else {
-			return getMinchaGedola30Minutes().compareTo(getMinchaGedolaBaalHatanya()) > 0 ? getMinchaGedola30Minutes()
-					: getMinchaGedolaBaalHatanya();
-		}
 	}
 
 	/**
