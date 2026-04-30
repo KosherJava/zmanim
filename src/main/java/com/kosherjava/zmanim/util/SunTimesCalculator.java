bccb7e1a1bc56e87f10336c7e4874af0b7b35097
Matthew Sheby <matthew.sheby@disney.com>
2026-04-29 22:36:08 -0700

    chore: update copyright year to 2026

--- src/main/java/com/kosherjava/zmanim/util/SunTimesCalculator.java
+++ src/main/java/com/kosherjava/zmanim/util/SunTimesCalculator.java
@@ -1,6 +1,6 @@
 /*
  * Zmanim Java API
- * Copyright (C) 2004-2025 Eliyahu Hershfeld
+ * Copyright (C) 2004-2026 Eliyahu Hershfeld
  *
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
